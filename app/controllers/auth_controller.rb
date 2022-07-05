8435b1681ddabe5fb39dc3144cd385d00c5f49d1
aladac <adam.ladachowski@gmail.com>
2022-07-05 13:07:11 +0200

    Google redirect

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
 class AuthController < ApplicationController
-  def login; end
+  def login
+    uri = URI('https://accounts.google.com/o/oauth2/v2/auth')
+
+    uri.query = {
+      client_id:     ENV.fetch('GOOGLE_CLIENT_ID', nil),
+      client_secret: ENV.fetch('GOOGLE_CLIENT_SECRET', nil),
+      redirect_uri:  ENV.fetch('GOOGLE_REDIRECT_URI', nil)
+    }.to_query
+
+    redirect_to uri.to_s, allow_other_host: true
+  end
 
   def logout; end
+
+  def callback
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'auth/login'
-  get 'auth/logout'
-  get 'welcome/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :auth do
+    get :login
+    get :logout
+    get :callback
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'welcome/index'
+
+  root 'welcome#index'
 end
